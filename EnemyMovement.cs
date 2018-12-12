142443b33953f85df3cfc8872c0fcfa46bba0d67
Eric Mazza <mcb6010@gmail.com>
2018-12-11 21:35:23 -0500

    Updated

---
+++ EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float moveSpeed;
+    private bool up = true;
+    private float timer = 0;
+    private float timer2 = 0;
+    public float timerMax;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer >= timerMax){
+            timer = 0;
+            up = false;
+        }
+        if (timer2 >= timerMax){
+            timer2 = 0;
+            up = true;
+        }
+
+        if (up){
+            timer++;
+            rb.velocity = Vector2.up * moveSpeed;
+            GetComponent<Animator>().SetBool("goingUp", true);
+        }else{
+            timer2++;
+            rb.velocity = Vector2.down * moveSpeed;
+            GetComponent<Animator>().SetBool("goingUp", false);
+        }
+	}
+}
--- MovementScript.cs
+++ MovementScript.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovementScript : MonoBehaviour {
 
     public GameObject dashEffect;
+    public GameObject dashEffectNormal;
+    public GameObject dashEffectLightning;
 
     private Rigidbody2D rb;
     public float moveSpeed;
     public float jumpHeight;
     public bool isGrounded;
     public float dashSpeed;
+    public float upwardDashSpeed;
     public float startDashTime;
     public float dashTime;
     private bool dashActive;
     private bool dashRestored;
+    private Vector2 upRight = (Vector2.right + Vector2.up);
+    private Vector2 upLeft = (Vector2.left + Vector2.up);
+    private int powerUp = 0;
+    /*
+     Power up numbers:
+     0: No Power Up
+     1: Lighting Power Up
+         */
 
     void Start(){
         rb = GetComponent<Rigidbody2D>();
@@ -24,18 +36,44 @@ public class MovementScript : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+
+        switch (powerUp){
+            case 0:
+                dashEffect = dashEffectNormal;
+                break;
+            case 1:
+                dashEffect = dashEffectLightning;
+                break;
+            default:
+                dashEffect = dashEffectNormal;
+                break;
+        }
         
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && dashRestored) {
             //transform.position += move * dashSpeed * Time.deltaTime;
-            if (move.x > 0 && dashActive == false){
+            if(move.x > 0 && dashActive == false && Input.GetKey(KeyCode.UpArrow)){ //Upright dash
+                GetComponent<Animator>().SetBool("isDashing", true);
+                rb.velocity = upRight * upwardDashSpeed;
+                Instantiate(dashEffect, transform.position, Quaternion.identity);
+                dashActive = true;
+                dashRestored = false;
+            }
+            if(move.x < 0 && dashActive == false && Input.GetKey(KeyCode.UpArrow)){ //Upleft dash
+                GetComponent<Animator>().SetBool("isDashing", true);
+                rb.velocity = upLeft * upwardDashSpeed;
+                Instantiate(dashEffect, transform.position, Quaternion.identity);
+                dashActive = true;
+                dashRestored = false;
+            }
+            if (move.x > 0 && dashActive == false){ //Right dash
                 GetComponent<Animator>().SetBool("isDashing", true);
                 rb.velocity = Vector2.right * dashSpeed;
                 Instantiate(dashEffect, transform.position, Quaternion.identity);
                 dashActive = true;
                 dashRestored = false;
             }
-            if(move.x < 0 && dashActive == false){
+            if(move.x < 0 && dashActive == false){ //Left dash
                 GetComponent<Animator>().SetBool("isDashing", true);
                 rb.velocity = Vector2.left * dashSpeed;
                 Instantiate(dashEffect, transform.position, Quaternion.identity);
@@ -81,6 +119,29 @@ public class MovementScript : MonoBehaviour {
 
     }
 
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.CompareTag("Thunder")){
+            Debug.Log("Thunder!");
+            powerUp = 1;
+        }
+        if (collision.gameObject.CompareTag("None")){
+            Debug.Log("No powers!");
+            powerUp = 0;
+        }
+
+        //Enemy Detection
+        if (collision.gameObject.CompareTag("enemy")){
+            if(powerUp == 1 && dashActive){
+                Debug.Log("Enemy Dead! " + dashActive);
+                Destroy(collision.gameObject);
+                Instantiate(dashEffect, transform.position, Quaternion.identity);
+            }
+            else{
+                Debug.Log("Player Dead!");
+                SceneManager.LoadScene("GameOver");
+            }
+         }
+        }
     void OnTriggerEnter2D(){
         isGrounded = true;
     }
@@ -90,6 +151,5 @@ public class MovementScript : MonoBehaviour {
     void OnTriggerExit2D(){
         isGrounded = false;
     }
-
 }
 
